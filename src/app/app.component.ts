1dfbef3c75f1f3b6f6591a599c85c895cc463c99
Alejandro Ruzafa <ruzafa-@outlook.es>
2023-11-18 19:47:46 +0100

    feat(translation): use navigator default language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private readonly translateService: TranslateService) {}
+
+  public ngOnInit(): void {
+    this.setNavigatorLanguage()
+  }
+
+  private setNavigatorLanguage(): void {
+    const language: string | undefined = this.translateService.getBrowserLang()
+    this.translateService.use(language || 'es')
+  }
 }
