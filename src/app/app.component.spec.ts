1ee395f578671f9c89da00d20ff9d5f09d1569c1
Alejandro Ruzafa <ruzafa-@outlook.es>
2023-11-18 19:57:50 +0100

    style: add missing types

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
-import { TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { AppComponent } from './app.component'
 
@@ -13,8 +13,8 @@ describe('AppComponent', () => {
   })
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent)
+    const app: AppComponent = fixture.componentInstance
     expect(app).toBeTruthy()
   })
 
--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core'
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  public items: Array<any> = [
-    { name: 'Maruja' },
-    { name: 'Pepe' },
-    { name: 'Paqui' },
-    { name: 'Antonia' },
-  ]
 }
