c5a982f838ece301602c311a89647534bef3b7c0
Alejandro Ruzafa <ruzafa-@outlook.es>
2023-11-18 21:44:42 +0100

    feat: set endpoint from env at service

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -2,19 +2,20 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Item } from '../interfaces/items/item'
 import { Observable } from 'rxjs'
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
 
-  private readonly URL_HOST: string = 'http://localhost:3000'
+  private readonly URL_HOST: string = environment.mercatechApi
 
   constructor(
     private readonly http: HttpClient,
   ) {}
 
-  public getItems(params: Partial<Item>): Observable<Array<Item>> {
+  public getItems(params: Partial<Item> = {}): Observable<Array<Item>> {
     return this.http.get<Array<Item>>(`${this.URL_HOST}/items`, { params })
   }
 
